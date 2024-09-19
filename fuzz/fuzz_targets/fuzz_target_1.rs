caf743c306f5690adefb4575362477d431b1bac0
Gly <gabin.lefranc@gmail.com>
2024-09-19 17:22:57 +0200

    Add fuzz struct Named for string

--- fuzz/fuzz_targets/fuzz_target_1.rs
+++ fuzz/fuzz_targets/fuzz_target_1.rs
@@ -1,12 +1,19 @@
 #![no_main]
 
-use std::{fmt::Display, ops::RangeBounds};
+use std::{fmt::Display, ops::{Deref, RangeBounds}};
 
 use libfuzzer_sys::fuzz_target;
 
+fuzz_target!(|data: Signature| {
+    let data_token = proc_macro2::TokenTree::Literal(proc_macro2::Literal::string(&data.to_string()));
+    let res = nu_signature_core::make_signature(data_token.clone().into());
+    let res = res.to_string();
+    assert!(!res.contains("compile_error"), "Failed to make signature:\nnu signature:\n{}\nTokenTree:\n{:?}\nRust signature:\n{}", data, data_token, res);
+});
+
 #[derive(arbitrary::Arbitrary, Debug)]
 struct Signature {
-    name: String,
+    name: Named,
     signature: Parameters,
 }
 
@@ -20,39 +27,6 @@ impl Display for Signature {
 struct Parameters {
     params: Vec<Parameter>,
     rest: Option<Parameter>,
-}
-
-fn char_from_ranges(index: usize, ranges: &[impl RangeBounds<char>]) -> char {
-    use std::ops::Bound;
-    let total = ranges
-        .iter()
-        .map(|r| {
-            if matches!((r.start_bound(), r.end_bound()), (Bound::Unbounded, _) | (_, Bound::Unbounded)) {
-                return None;
-            }
-            let start = match r.start_bound() {
-                Bound::Included(c) => *c as u32,
-                Bound::Excluded(c) => *c as u32 + 1,
-                Bound::Unbounded => return None
-            };
-            let end = match r.end_bound() {
-                Bound::Included(c) => *c as u32 + 1,
-                Bound::Excluded(c) => *c as u32,
-                Bound::Unbounded => return None
-            };
-            Some((end - start) as usize)
-        })
-        .sum::<Option<usize>>();
-    let index = if let Some(total) = total { index % total } else { index };
-    for (i, range) in ranges.iter().enumerate() {
-        let start = range.start_bound();
-        let end = range.end_bound();
-        if index < (end - start) as usize {
-            return *start + (index as u32);
-        }
-        index -= (end - start) as usize;
-    }
-    'a'
 }
 
 impl Display for Parameters {
@@ -70,8 +44,8 @@ impl Display for Parameters {
 
 #[derive(arbitrary::Arbitrary, Debug)]
 struct Parameter {
-    name: String,
-    desc: Option<String>,
+    name: Named,
+    desc: Option<Named>,
     ty: ParameterType,
 }
 
@@ -105,7 +79,7 @@ enum ParameterType {
 struct ParameterData {
     type_: DataType,
     optional: bool,
-    default_value: Option<String>,
+    default_value: Option<Named>,
 }
 
 impl Display for ParameterData {
@@ -157,8 +131,72 @@ impl Display for DataType {
     }
 }
 
-fuzz_target!(|data: Signature| {
-    let res = nu_signature_core::make_signature(proc_macro2::TokenTree::Literal(proc_macro2::Literal::string(&data.to_string())).into());
-    let res = res.to_string();
-    assert!(res.contains("compile_error"));
-});
+fn char_from_ranges(index: u32, ranges: &[impl RangeBounds<char>]) -> char {
+    use std::ops::Bound;
+    assert!(!ranges.is_empty() && !ranges.iter().any(|r| matches!((r.start_bound(), r.end_bound()), (Bound::Unbounded, _) | (_, Bound::Unbounded))), "Invalid range");
+    let ranges_numb = ranges
+        .iter()
+        .map(|r| {
+            let start = match r.start_bound() {
+                Bound::Included(c) => *c as u32,
+                Bound::Excluded(c) => *c as u32 + 1,
+                Bound::Unbounded => unreachable!("Invalid range"),
+            };
+            let end = match r.end_bound() {
+                Bound::Included(c) => *c as u32 + 1,
+                Bound::Excluded(c) => *c as u32,
+                Bound::Unbounded => unreachable!("Invalid range"),
+            };
+            (start, end)
+        });
+    assert!(!ranges_numb.clone().any(|(start, end)| start > end), "Invalid range");
+    let total = ranges_numb.clone().fold(0, |acc, (start, end)| {
+        acc + (end - start)
+    });
+    let mut index = index % total;
+    for (start, end) in ranges_numb {
+        if index < (end - start) {
+            return unsafe { char::from_u32_unchecked(start + index) };
+        }
+        index -= end - start;
+    }
+    'a'
+}
+
+#[derive(Debug)]
+struct Named(String);
+
+impl arbitrary::Arbitrary<'_> for Named {
+    fn arbitrary(u: &mut arbitrary::Unstructured<'_>) -> arbitrary::Result<Self> {
+        if u.is_empty() {
+            return Err(arbitrary::Error::NotEnoughData);
+        }
+        let name = u.arbitrary_iter::<u32>()?
+            .take(20)
+            .enumerate()
+            .map(|(i, c)| {
+                if i == 0 {
+                    Ok(char_from_ranges(c?, &['a'..='z', 'A'..='Z']))
+                } else {
+                    Ok(char_from_ranges(c?, &['a'..='z', 'A'..='Z', '0'..='9']))
+                }
+            }
+        );
+        let name = name.collect::<Result<String, _>>()?;
+        if name.len() < 2 {
+            return Err(arbitrary::Error::NotEnoughData);
+        }
+        Ok(Named(name))
+    }
+}
+impl Deref for Named {
+    type Target = str;
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+impl Display for Named {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.0)
+    }
+}
